596a17847d1e59f105ce453609b5d78cc53b84a8
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2014-04-16 16:45:28 +0900

    トラッキングコードを追加

--- skin/pukiwiki.skin.php
+++ skin/pukiwiki.skin.php
@@ -283,6 +283,14 @@ function _toolbar($key, $x = 20, $y = 20){
  <?php echo S_COPYRIGHT ?>.
  Powered by PHP <?php echo PHP_VERSION ?>. HTML convert time: <?php echo $taketime ?> sec.
 </div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', '', '');
+  ga('send', 'pageview');
+</script>
 </body>
 </html>
